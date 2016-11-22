ad21dd8e52a2e39bbb46df48ba9bdef91bce6832
jake-freeman <jfreeman@hawk.iit.edu>
2016-11-21 20:55:17 -0600

    Add more titles, parse command line args

--- buzz.js
+++ buzz.js
@@ -2,6 +2,6 @@ var MarkovChain = require('markovchain')
   , fs = require('fs')
   , titles = new MarkovChain(fs.readFileSync('./titles.dat', 'utf8'))
 
-generated_title = titles.end(20).process()
+generated_title = titles.end(15).start(process.argv[2] ? process.argv[2] : '').process();
 
 console.log(generated_title)
